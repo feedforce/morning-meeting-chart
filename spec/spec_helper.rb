cf3b4c4ee9112a59830e2db61217dd1ba175a938
pokotyamu <pokotyamu@gmail.com>
2016-07-04 21:27:26 +0900

    FactoryGirl のシンタックスを省略する

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,8 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+  require 'factory_girl_rails'
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
@@ -96,4 +98,7 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+
+  #FactoryGirl を使うときに記述量を省略して書けるようにする
+  config.include FactoryGirl::Syntax::Methods
 end
