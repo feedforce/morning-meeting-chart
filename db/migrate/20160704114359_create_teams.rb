8f62255cada94aebf7dcd38e664d8a89f85f3966
pokotyamu <pokotyamu@gmail.com>
2016-07-04 20:44:14 +0900

    $ rails g scaffold team name:string goal:integer

---
+++ db/migrate/20160704114359_create_teams.rb
@@ -0,0 +1,10 @@
+class CreateTeams < ActiveRecord::Migration[5.0]
+  def change
+    create_table :teams do |t|
+      t.string :name
+      t.integer :goal
+
+      t.timestamps
+    end
+  end
+end
