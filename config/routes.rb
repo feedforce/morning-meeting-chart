6599b35457406a36740bffc0350bd8011751f2d5
pokotyamu <pokotyamu@gmail.com>
2016-07-04 20:49:53 +0900

    ルーティングを親子関係に変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :topics
-  resources :progresses
-  resources :teams
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :teams do
+    resources :progresses do
+      resources :topics
+    end
+  end
 end
