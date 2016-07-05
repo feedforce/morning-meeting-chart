d906804608b22b94df23e2efa29347b4e51afcc0
pokotyamu <pokotyamu@gmail.com>
2016-07-05 21:17:09 +0900

    `Progress` モデルにバリデーションを作成

--- app/models/progress.rb
+++ app/models/progress.rb
@@ -1,3 +1,6 @@
 class Progress < ApplicationRecord
   belongs_to :team
+
+  validates :start_date, presence: true
+  validates :amount, presence: true
 end
--- spec/models/progress_spec.rb
+++ spec/models/progress_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe Progress, type: :model do
   describe 'リレーション' do
     it { should belong_to(:team) }
   end
+
+  describe 'バリデーション' do
+    it { should validate_presence_of(:start_date) }
+    it { should validate_presence_of(:amount) }
+  end
 end
