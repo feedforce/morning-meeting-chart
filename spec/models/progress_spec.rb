1b7506eca3badc2f686f9d0011986eca788b774a
pokotyamu <pokotyamu@gmail.com>
2016-07-05 21:14:19 +0900

    リレーションについてのテストを追加

--- spec/models/progress_spec.rb
+++ spec/models/progress_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Progress, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'リレーション' do
+    it { should belong_to(:team) }
+  end
 end
