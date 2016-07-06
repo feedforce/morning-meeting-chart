83a4086275a7cbbb6b79488daa31c9b64b0c579e
pokotyamu <pokotyamu@gmail.com>
2016-07-06 10:29:39 +0900

    ミニマムで `/teams/:tema_id/progresses` のルーティングを作成

--- app/controllers/progresses_controller.rb
+++ app/controllers/progresses_controller.rb
@@ -1,2 +1,4 @@
 class ProgressesController < ApplicationController
+  def index
+  end
 end
--- spec/requests/progresses_spec.rb
+++ spec/requests/progresses_spec.rb
@@ -1,10 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "Progresses", type: :request do
-  describe "GET /progresses" do
-    it "works! (now write some real specs)" do
-      get progresses_index_path
-      expect(response).to have_http_status(200)
-    end
+  describe 'GET /teams/:team_id/progress' do
+    subject { get "/teams/#{team.id}/progresses" }
+    let(:team) { create(:team) }
+    it_behaves_like 'HTTP 200 OK'
   end
 end
