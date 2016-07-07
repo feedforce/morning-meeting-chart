9778030d9aec58cc952ee9f000d5b9a1ebbfd447
pokotyamu <pokotyamu@gmail.com>
2016-07-07 21:49:49 +0900

    factory と seed を設定

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+team = FactoryGirl.create(:team)
+4.times do
+  pr = FactoryGirl.create(:progress, team: team)
+  FactoryGirl.create(:topic, progress: pr)
+end
