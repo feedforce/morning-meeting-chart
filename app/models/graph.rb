2e46515b274bd0256bd13e0d69a59f6c9c7e67f3
pokotyamu <pokotyamu@gmail.com>
2016-07-12 00:35:25 +0900

    `$ rails g model graph team:references  yaxis:string`

---
+++ app/models/graph.rb
@@ -0,0 +1,3 @@
+class Graph < ApplicationRecord
+  belongs_to :team
+end
---
+++ spec/factories/graphs.rb
@@ -0,0 +1,8 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :graph do
+    team nil
+    yaxis "MyString"
+  end
+end
