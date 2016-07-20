4b8faf7d2ac57a8fd52ff932944d3e86086a42e9
pokotyamu <pokotyamu@gmail.com>
2016-07-20 20:18:50 +0900

    public 以下にfont 移動した

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,6 +8,7 @@ precompile_target = lambda do |filename, path|
   puts filename
   path =~ /app\/assets/ && !%w(.js .css .html .json .eot .svg .ttf .woff .woff2).include?(File.extname(filename))
 end
+
 Rails.application.config.assets.precompile = [
   precompile_target,
   /(?:\/|\\|\A)application\.(css|js)$/
