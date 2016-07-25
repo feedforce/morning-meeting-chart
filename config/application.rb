e39efdb6885bceed8265621666be577f8dbb7def
otofu-square <yukiyakonkon1024@gmail.com>
2016-07-25 21:01:08 +0900

    Change path

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module MorningMeetingChart
   class Application < Rails::Application
     config.assets.paths << "#{Rails}/vendor/assets/fonts"
-    config.autoload_paths += %W(#{config.root}/lib)
+    config.autoload_paths += "#{Rails}/lib"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
