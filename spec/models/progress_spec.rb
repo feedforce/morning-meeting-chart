c62c58dd4e5b4e4e3c568aa1268cfd13aa17806c
pokotyamu <pokotyamu@gmail.com>
2016-07-28 21:51:21 +0900

    不要なテストを削除

--- spec/models/progress_spec.rb
+++ spec/models/progress_spec.rb
@@ -1,10 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Progress, type: :model do
-  describe 'リレーション' do
-    it { should belong_to(:team) }
-  end
-
   describe 'バリデーション' do
     it { should validate_presence_of(:start_date) }
     it { should validate_presence_of(:amount) }
