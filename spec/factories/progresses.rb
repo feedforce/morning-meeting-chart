b58d8b15d59fdf85bf6513b0c8f91e1ddbd5099a
pokotyamu <pokotyamu@gmail.com>
2016-07-28 21:42:54 +0900

    Progress の Factory を修正

--- spec/factories/progresses.rb
+++ spec/factories/progresses.rb
@@ -2,7 +2,9 @@
 
 FactoryGirl.define do
   factory :progress do
+    goal
     amount { rand(5) + 5 }
     sequence(:start_date) { |i| (Date.today - (Date.today.wday - 1) - 7) + i.weeks }
+    end_date { start_date + 4.days }
   end
 end
