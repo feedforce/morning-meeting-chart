142dcec4990c27f91761559b8a7a3246ea7e56b6
pokotyamu <pokotyamu@gmail.com>
2016-08-01 17:13:30 +0900

    目標一覧画面の作成

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,2 +1,13 @@
 class GoalsController < ApplicationController
+  before_action :set_team
+
+  def index
+    @goals = @team.goals
+  end
+
+  private
+
+  def set_team
+    @team = Team.find(params[:team_id])
+  end
 end
