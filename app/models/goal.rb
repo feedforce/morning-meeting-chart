d750ef18dc1d4b4be074b1ea112f7b37e0160cc5
pokotyamu <pokotyamu@gmail.com>
2016-08-01 18:12:41 +0900

    目標一覧に今月の目標値を追加

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -4,4 +4,22 @@ class Goal < ApplicationRecord
 
   validates :date, presence: true
   validates :goal, presence: true
+
+  def goal_formatted
+    if team.orders?
+      ActionController::Base.helpers.number_to_currency(
+        goal,
+        format: "%n%u",
+        unit: ' 件',
+        precision: 0
+      )
+    else
+      ActionController::Base.helpers.number_to_currency(
+        goal,
+        format: "%n%u",
+        unit: ' 円',
+        precision: 0
+      )
+    end
+  end
 end
