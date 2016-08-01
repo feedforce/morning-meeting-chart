d81c3fcb1345986f593f074d965f2d087ad4f94d
pokotyamu <pokotyamu@gmail.com>
2016-08-01 17:45:38 +0900

    新規目標作成ページ

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,13 +1,47 @@
 class GoalsController < ApplicationController
   before_action :set_team
+  before_action :set_goal, only: [:edit]
+  before_action :set_entity, only: [:new, :edit]
 
   def index
     @goals = @team.goals
+  end
+
+  def new
+    @goal = @team.goals.new
+  end
+
+  def create
+    goal = @team.goals.new(goal_params)
+
+    respond_to do |format|
+      if goal.save
+        format.html { redirect_to team_goals_path(@team), notice: '新しい目標が作成されました' }
+      else
+        format.html { render :new }
+      end
+    end
   end
 
   private
 
   def set_team
     @team = Team.find(params[:team_id])
+  end
+
+  def set_goal
+    @goal = Goal.find(params[:id])
+  end
+
+  def set_entity
+    if @team.order?
+      @entity = '件'
+    else
+      @entity = '円'
+    end
+  end
+
+  def goal_params
+    params.require(:goal).permit(:date, :goal)
   end
 end
