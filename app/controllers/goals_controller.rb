c900fdc690744dc20b237869064dab5e05c7b7d3
pokotyamu <pokotyamu@gmail.com>
2016-08-02 18:34:22 +0900

    Goal の年月で選択するように変更

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -62,6 +62,8 @@ class GoalsController < ApplicationController
   end
 
   def goal_params
-    params.require(:goal).permit(:date, :goal)
+    param = params.require(:goal).permit(:date, :goal)
+    param[:date] += '-01' if param[:date]
+    param
   end
 end
