308fd2f796c80df43d64be4338246bf8e3ee9f0f
pokotyamu <pokotyamu@gmail.com>
2016-08-02 10:57:00 +0900

    保存するタイミングで Goal を自動追加
    * もし Goal が存在しない場合は 7 月分で自動作成

--- app/controllers/progresses_controller.rb
+++ app/controllers/progresses_controller.rb
@@ -1,6 +1,7 @@
 class ProgressesController < ApplicationController
-  before_action :set_team, only: [:index]
+  before_action :set_team, only: [:index, :update, :create]
   before_action :set_progress, only: [:edit, :update, :destroy]
+  before_action :set_last_goal, only: [:update, :create]
 
   def index
     @graph = @team.graph(graph_params)
@@ -72,7 +73,7 @@ class ProgressesController < ApplicationController
   end
 
   def progress_params
-    params.require(:progress).permit(:amount, :start_date, :end_date).merge(team: Team.find(params[:team_id]))
+    params.require(:progress).permit(:amount, :start_date, :end_date).merge(team: Team.find(params[:team_id]), goal: @goal)
   end
 
   def topic_params
@@ -85,6 +86,15 @@ class ProgressesController < ApplicationController
       today - (today.wday - 1)
     else
       today - (8 - today.wday)
+    end
+  end
+
+  def set_last_goal
+    last_goal = @team.goals.last
+    if last_goal
+      @goal = last_goal
+    else
+      @goal = Goal.create(date: Date.new(2016, 7, 1, 0), goal: @team.goal, team_id: @team.id)
     end
   end
 end
