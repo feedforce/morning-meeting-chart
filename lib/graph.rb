3a51e8946af6ece2b21bc8a22412b9908fee2799
pokotyamu <pokotyamu@gmail.com>
2016-08-08 15:05:06 +0900

    Add `stacked_data_line`

--- lib/graph.rb
+++ lib/graph.rb
@@ -1,21 +1,36 @@
 module Graph
-  attr_accessor :team, :goals
+  attr_accessor :team, :goals, :series
 
   def self.create(goals)
     return LazyHighCharts::HighChart.new('graph') unless goals
 
     # NOTE: 目標が１つの時は配列の１要素に変換する
     goals = [goals] if goals.instance_of?(Goal)
-    @team = goals.first.team
-    @goals = goals
-
+    init(goals)
     LazyHighCharts::HighChart.new('graph') do |f|
       f.title(text: title)
       f.xAxis(categories: categories)
+      f.series(
+        type: 'line',
+        name: '合計値',
+        data: stacked_data_line,
+        dataLabels: {
+          enabled: true
+        }
+      )
     end
   end
 
-  private
+  def self.init(goals)
+    @team = goals.first.team
+    @goals = goals
+    set_series
+  end
+
+  def self.set_series
+    progresses = @goals.flat_map(&:progresses).sort_by!(&:start_date)
+    @series = progresses.map(&:amount)
+  end
 
   def self.title
     title = "#{@team.name} : #{@goals.first.date.year} 年 #{@goals.first.date.month} 月"
@@ -36,5 +51,12 @@ module Graph
 
   def self.category_day(progress)
     "#{progress.start_date.day}〜#{(progress.end_date).day}日"
+  end
+
+  def self.stacked_data_line
+    sum = 0
+    @series.map do |s|
+      sum += s
+    end
   end
 end
