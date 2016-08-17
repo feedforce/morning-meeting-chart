0b8f4c6db3021ed8d1a39fd74b4c94d95dfe2987
hanocha <saiki@feedforce.jp>
2016-08-17 15:17:22 +0000

    とりあえずShowができるようになった
    前後の共有事項を検索して、リンクボタンの有無を制御できるところまで。

--- app/models/shared_info.rb
+++ app/models/shared_info.rb
@@ -2,4 +2,19 @@ class SharedInfo < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
   validates :announce_date, presence: true
+
+  scope :prev_info, -> (announce_date, id) {
+    where('"announce_date" = ? AND "id" < ?', announce_date, id).order(:id).last
+  }
+  scope :next_info, -> (announce_date, id) {
+    where('"announce_date" = ? AND "id" > ?', announce_date, id).order(:id).first
+  }
+
+  def self.has_prev_info?(announce_date, id)
+    exists?(['"announce_date" = ? AND "id" < ?', announce_date, id])
+  end
+
+  def self.has_next_info?(announce_date, id)
+    exists?(['"announce_date" = ? AND "id" > ?', announce_date, id])
+  end
 end
