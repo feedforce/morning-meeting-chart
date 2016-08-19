d3ab557ceda756ee098e6308d0758452f7d8e0aa
otofu-square <yukiyakonkon1024@gmail.com>
2016-08-19 19:44:28 +0900

    Fix a problem

--- lib/qiita_exporter.rb
+++ lib/qiita_exporter.rb
@@ -3,44 +3,53 @@ require 'qiita'
 require 'erb'
 
 class QiitaExporter
-  def self.export
+  def self.exec
     @@instance ||= self.new
-    @@instance.client.create_item(params)
+    @@instance.client.create_item(@@instance.params)
   end
 
-  def self.client
-    @client ||= Qiita::Client.new(access_token: ENV['QIITA_ACCESS_TOKEN'])
+  def client
+    @client ||= Qiita::Client.new(
+      access_token: ENV['QIITA_ACCESS_TOKEN'],
+      team: ENV['QIITA_TEAM_NAME']
+    )
   end
 
-  def self.params
+  def params
     {
       title: title,
-      tags: '月曜全体朝会',
+      tags: tags,
       body: body,
       coediting: true
     }
   end
 
-  def self.title
+  def tags
+    [
+      { name: '朝会まとめ', versions: [] }
+    ]
+  end
+
+  def title
     now = Time.current.in_time_zone('Asia/Tokyo')
     %(#{now.year}/#{now.month}/#{now.day} 月曜全体朝会まとめ)
   end
 
-  def self.body
+  def body
     ERB.new(File.read('./lib/template.md'), nil, '-').result(binding)
   end
 
-  def self.progress(team)
+  def progress(team)
     goal = team.goals.last
     sum = goal.progresses.sum(:amount)
     "今月の達成度 : #{sum}/#{goal.goal} #{entity(team)} (#{((sum.to_f/goal.goal.to_f) * 100).to_i}%)"
   end
 
-  def self.topics(team)
+  def topics(team)
     Topic.where(progress_id: team.goals.last.progresses.last.id)
   end
 
-  def self.entity(team)
+  def entity(team)
     return '(件)' if team.orders?
     return '(円)' if team.sales?
   end
--- lib/template.md
+++ lib/template.md
@@ -4,12 +4,14 @@
 ### <%= progress(team) %>
 
 <TODO: ここにグラフを出力して貼ってね>
+
   <%- topics = topics(team) -%>
   <%- if topics.present? -%>
 ### 先週のトピック
 
     <%- topics.each do |topic| -%>
 - <%= topic.content %>
+
     <%- end -%>
   <%- end -%>
 <%- end -%>
