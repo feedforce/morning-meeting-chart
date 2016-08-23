ef3d95143bd5e0aa7cdfd9a231c24cbf84e78e7a
Hanocha <saiki@feedforce.jp>
2016-08-23 19:56:00 +0900

    autolink を有効化した

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -2,7 +2,7 @@ module MarkdownHelper
   def markdown(text)
     unless @markdown
       renderer = Redcarpet::Render::HTML.new(hard_wrap: true)
-      @markdown = Redcarpet::Markdown.new(renderer)
+      @markdown = Redcarpet::Markdown.new(renderer, autolink: true, tables: true)
     end
 
     @markdown.render(text).html_safe
