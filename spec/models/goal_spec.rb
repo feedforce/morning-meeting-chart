d343a7a0fcaa7c9244c3682f6d5dbf0a0557e6b8
pokotyamu <pokotyamu@gmail.com>
2016-09-12 20:46:15 +0900

    Add validation for `Goal#goal`.

--- spec/models/goal_spec.rb
+++ spec/models/goal_spec.rb
@@ -17,5 +17,13 @@ RSpec.describe Goal, type: :model do
     describe '#date' do
       it { is_expected.to validate_presence_of(:date) }
     end
+
+    describe '#goal' do
+      it { is_expected.to validate_presence_of(:goal) }
+      it '値が1以上であること' do
+        is_expected.to validate_numericality_of(:goal)
+                        .only_integer.is_greater_than_or_equal_to(1)
+      end
+    end
   end
 end
