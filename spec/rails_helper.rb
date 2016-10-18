e5cc661de1c860f6ec1ab6d5cbc4cefc22233e27
pokotyamu <pokotyamu@gmail.com>
2016-10-18 20:49:55 +0900

    時間関係のテストを書くために Helper を追加

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -55,6 +55,8 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
   config.include FactoryGirl::Syntax::Methods
+  config.include ActiveJob::TestHelper
+  config.include ActiveSupport::Testing::TimeHelpers
 end
 
 # spec/support 以下の .rb を全て require する
