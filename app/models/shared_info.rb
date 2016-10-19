17c6f2e42c1bb636c0f23b7af2144b7112195944
pokotyamu <pokotyamu@gmail.com>
2016-10-19 10:15:34 +0900

    Fix rubocop.

--- app/models/shared_info.rb
+++ app/models/shared_info.rb
@@ -10,23 +10,22 @@
 #  created_at    :datetime         not null
 #  updated_at    :datetime         not null
 #
-
 class SharedInfo < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
   validates :announce_date, presence: true
 
-  scope :this_period, -> {
-    where('? < announce_date AND announce_date <= ?', Date.today - 7, Date.today)
-  }
+  scope :this_period, -> do
+    where('? < announce_date AND announce_date <= ?', Date.today - 7, Date.today).order(:id)
+  end
 
-  scope :prev_id, -> (id) {
+  scope :prev_id, -> (id) do
     where('id < ?', id).order(:id).last
-  }
+  end
 
-  scope :next_id, -> (id) {
+  scope :next_id, -> (id) do
     where('id > ?', id).order(:id).first
-  }
+  end
 
   def self.has_prev_info?(id)
     exists?(['? < announce_date AND announce_date <= ? AND "id" < ?', Date.today - 7, Date.today, id])
