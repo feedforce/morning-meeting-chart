f46a6b2367eebeaae9742c363aee86d6b4d05d6e
pokotyamu <pokotyamu@gmail.com>
2016-10-26 17:37:22 +0900

    Add `HolidayCalendar#end_week_day`.

--- app/models/holiday_calendar.rb
+++ app/models/holiday_calendar.rb
@@ -6,4 +6,12 @@ module HolidayCalendar
       start_date += 1.days
     end
   end
+
+  def self.end_week_day(date)
+    end_date = date - (date.wday - 5)
+    loop do
+      return end_date unless HolidayJp.holiday?(end_date)
+      end_date -= 1.days
+    end
+  end
 end
--- spec/models/holiday_calendar_spec.rb
+++ spec/models/holiday_calendar_spec.rb
@@ -31,4 +31,35 @@ RSpec.describe HolidayCalendar, type: :lib do
       end
     end
   end
+
+  describe '.end_week_day' do
+    subject { HolidayCalendar.end_week_day(date) }
+
+    context '金曜日が祝日ではない場合' do
+      # NOTE: 2016年10月28日(金) は平日
+      let(:date) { Date.new(2016, 10, 26) }
+
+      it '金曜日の日付が返る' do
+        expect(subject).to eq Date.new(2016, 10, 28)
+      end
+    end
+
+    context '金曜日が祝日の場合' do
+      # NOTE: 2016年12月23日(金) は祝日
+      let(:date) { Date.new(2016, 12, 21) }
+
+      it '木曜日の日付が返る' do
+        expect(subject).to eq Date.new(2016, 12, 22)
+      end
+    end
+
+    context '金曜日から祝日が続いている場合' do
+      # NOTE: 2017年05月03日(水) ~ 2017年05月05日(金) は祝日
+      let(:date) { Date.new(2017, 5, 4) }
+
+      it '１番最初の平日を返す' do
+        expect(subject).to eq Date.new(2017, 5, 2)
+      end
+    end
+  end
 end
