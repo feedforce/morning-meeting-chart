c1c9c042a8c0ddedc16d2a66c0913b101dada71e
hanocha <saiki@feedforce.jp>
2016-11-03 23:30:40 +0900

    デプロイ先サーバの情報をベタ書き
    SSH設定を書いていないのでとりあえず仮設定

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,6 +6,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
+server '54.249.16.106', user: 'ec2-user', roles: %w{app}
 
 
 
