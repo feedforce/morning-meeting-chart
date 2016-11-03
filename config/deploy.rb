41a25f89679b4f2ea940139e52e3534dc1fe1455
hanocha <saiki@feedforce.jp>
2016-11-03 21:29:44 +0900

    リポジトリ周りの設定をした
    config/deploy.rbの修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,17 +1,17 @@
 # config valid only for current version of Capistrano
 lock '3.6.1'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'morning-meeting-chart'
+set :repo_url, 'git@github.com:feedforce/morning-meeting-chart.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, '/var/www/morning-meeting-chart'
 
 # Default value for :scm is :git
-# set :scm, :git
+set :scm, :git
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
